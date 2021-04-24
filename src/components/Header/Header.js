fb02f4e3b1d37ecb2104c8dbd673b64dbae48e1d
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-04-24 16:20:21 +0200

    Add and style header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='container header-container'>
+                <h1 className='header-title'>Weather App</h1>
+            </div>       
+        </header>
+    );
+}
+
+export default Header;
