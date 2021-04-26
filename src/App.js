4f21a083a864187efc71640c7b6b76f29b36bcc2
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-04-26 15:55:19 +0200

    Add main section to wrap smaller elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import MainSection from './components/MainSection/MainSection';
 
 function App() {
   return (
     <>
     <Header />
+    <MainSection />
     </>
   );
 }
---
+++ src/components/MainSection/MainSection.js
@@ -0,0 +1,11 @@
+
+
+const MainSection = () => {
+    return (
+        <div className="container main-section-container">
+            
+        </div>
+    );
+}
+
+export default MainSection;
