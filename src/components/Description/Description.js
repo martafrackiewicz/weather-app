dbaf6d67cbd201f99d4de68d3ba5c4af49ec1ad0
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-04-27 18:41:39 +0200

    Display current date

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,10 +1,16 @@
 import "./Description.scss";
 
 const Description = ({name}) => {
+
+    const date = new Date();
+    const dateLocale = new Date().toLocaleDateString();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayIndex = date.getDay();
+
     return (
         <div className="description-container">
             <p className="description-city">{name}</p>
-            <p className="description-date">Friday, 16 sierpnia 2021 roku</p>
+            <p className="description-date">{days[dayIndex]}, {dateLocale}</p>
         </div>
     );
 }
